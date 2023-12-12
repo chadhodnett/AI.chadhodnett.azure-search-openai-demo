aeb5b6ef34aeeec52a182789874594d2162abc0a
Chad Hodnett <chad.hodnett@nucor.com>
2023-12-12 15:52:26 +0000

    Customize for PSI docs

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -7,14 +7,15 @@ export type ExampleModel = {
     value: string;
 };
 
-const EXAMPLES: ExampleModel[] = [
-    {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
-    },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
-];
+const EXAMPLES: ExampleModel[] = [];
+// const EXAMPLES: ExampleModel[] = [
+//     {
+//         text: "What is included in my Northwind Health Plus plan that is not in standard?",
+//         value: "What is included in my Northwind Health Plus plan that is not in standard?"
+//     },
+//     { text: "What happens in a performance review?", value: "What happens in a performance review?" },
+//     { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+// ];
 
 interface Props {
     onExampleClicked: (value: string) => void;
